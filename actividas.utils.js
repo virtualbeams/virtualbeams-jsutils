11db200d8d6d6e8a4f838de86a0eb3ef100a2e64
jpablo1012 <jpablo101296@gmail.com>
2015-03-02 22:14:45 -0500

    add Array.create

--- actividas.utils.js
+++ actividas.utils.js
@@ -56,4 +56,18 @@
 		array.insert(index, item);
 	};
 
+	Array.create = function(length) {
+		var arr = new Array(length || 0);
+		i = length;
+
+		if(arguments.length > 1) {
+			var args = Array.prototype.slice.call(arguments, 1);
+			while(i--) {
+				arr[length - 1 - i] = Array.create.apply(this, args);
+			}
+		}
+
+		return arr;
+	};
+
 })();
